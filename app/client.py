ee848d0462ed2fdc75d68adb7991978e4133ceee
sdkinfotech <antonsedykh555@gmail.com>
2024-03-21 07:08:07 +0300

    DEV:main.py: Добавлен пример работы запроса дневных или недельных свечей; DEV:./app/client.py: Добавлен метод для получения дневных или недельных свечей по инструменту в указанном режиме торгов, например TQBR

--- app/client.py
+++ app/client.py
@@ -1,36 +1,57 @@
-"""
-Асинхронный клиент API для взаимодействия с Trade API Finam.
+import aiohttp #  для асинхронных HTTP-запросов
 
-Этот модуль предоставляет асинхронный клиент API, который может использоваться
-для выполнения различных API запросов к Trade API Finam,
-включая проверку токенов доступа и другие операции торговли и управления портфелем.
-
-Примеры включают асинхронную отправку запросов для получения информации о свечах,
-заказах и операциях со стоп-заявками.
-"""
-
-import aiohttp
+# Определяем класс TradeAPIClient, который будет клиентом API.
 class TradeAPIClient:
     """
     Клиент для взаимодействия с Trade API Finam.
     Позволяет выполнять асинхронные запросы к API для проверки токенов доступа.
     """
     
+    # Конструктор класса, принимает два параметра: токен API и базовый URL API.
     def __init__(self, api_token: str, api_url: str):
         """
         Инициализация клиента API.
         :param api_token: Ключ API для доступа к сервису.
+        :param api_url: Базовый URL API, к которому будут отправляться запросы.
         """
+        # Сохраняем полученные значения в атрибутах объекта класса.
         self.base_url = api_url
         self.headers = {"accept": "text/plain", "X-Api-Key": api_token}
 
+    # Асинхронный метод для проверки токена доступа.
     async def check_access_token(self) -> str:
         """
         Асинхронная проверка токена доступа.
-        :return: Ответ сервера о состоянии токена.
+        Возвращает ответ сервера о состоянии токена в текстовом формате.
         """
+        # Формируем полный URL для запроса, добавляя к базовому URL путь к эндпоинту.
         url = f"{self.base_url}/access-tokens/check"
+        # Создаем асинхронную сессию запросов.
+        async with aiohttp.ClientSession() as session:
+            # Отправляем GET-запрос, используя сессию и указанные заголовки.
+            async with session.get(url, headers=self.headers) as response:
+                # Возвращаем текст ответа сервера.
+                return await response.text()
+    
+    # Асинхронный метод для получения информации о дневных свечах.
+    async def get_day_candles(
+            self, 
+            security_board: str, 
+            security_code: str, 
+            time_frame: str, 
+            interval_from: str, 
+            interval_to: str, 
+            interval_count: int) -> str:
+        """
+        Асинхронный запрос информации о дневных свечах.
+        Принимает параметры запроса и возвращает ответ сервера с данными о свечах в текстовом формате.
+        """
+        # Формируем URL запроса, включая необходимые параметры как часть строки запроса (query string).
+        url = (f"{self.base_url}/day-candles?SecurityBoard={security_board}&"
+               f"SecurityCode={security_code}&TimeFrame={time_frame}&Interval.From={interval_from}&"
+               f"Interval.To={interval_to}&Interval.Count={interval_count}")
+        # Отправляем асинхронный GET-запрос с указанными параметрами и заголовками.
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=self.headers) as response:
-                # Возможна обработка статуса ответа, например, response.status
+                # Возвращаем текстовый ответ сервера.
                 return await response.text()
--- main.py
+++ main.py
@@ -1,20 +1,49 @@
-import asyncio
-from dotenv import load_dotenv
-import os
-from app.client import TradeAPIClient  # Импортируйте вашего клиента API соответственно
-import app.settings as settings
 
-load_dotenv()  # Загрузите переменные среды
+import asyncio # для асинхронных приложений
+from dotenv import load_dotenv # для загрузки переменных окружения из .env файла.
+import os # для работы с операционной системой, включая переменные окружения.
+from app.client import TradeAPIClient # для взаимодействия с Trade API Finam.
+import app.settings as settings # Импорт настроек приложения
 
+# Загрузка переменных окружения из файла .env
+load_dotenv()  
+
+# Определение асинхронной главной функции main.
 async def main():
     """
-    Главная асинхронная функция для проверки работы клиента API.
+    Главная асинхронная функция для проверки работы клиента API, 
+    включая запрос информации о дневных свечах.
+    Эта функция демонстрирует два основных использования клиента: проверку токена
+    и получение информации о дневных свечах для заданного инструмента.
     """
+    # Получение токена API из переменных окружения.
     API_TOKEN = os.getenv("API_TOKEN")
-    # Используйте переменную окружения для токена
+    # Создание экземпляра клиента API с использованием токена и URL из настроек.
     client = TradeAPIClient(api_token=API_TOKEN, api_url=settings.API_URL)
+    
+    # Выполнение запроса проверки токена доступа и вывод ответа.
     token_check = await client.check_access_token()
-    print(token_check)
+    print(f"Проверка токена доступа: {token_check}")
 
+    # Параметры запроса информации о дневных свечах.
+    security_board = "TQBR"  # Режим торговой площадки.
+    security_code = "GAZP"   # Тикер трейдовой бумаги.
+    time_frame = "D1"        # Временной интервал для свечей.
+    interval_from = "2024-03-01"  # Начальная дата интервала.
+    interval_to = "2024-03-21"    # Конечная дата интервала.
+    interval_count = 0       # Количество запрашиваемых свечей (0 для всех доступных).
+    
+    # Выполнение запроса получения информации о дневных свечах и вывод ответа.
+    day_candles_info = await client.get_day_candles(
+        security_board=security_board, 
+        security_code=security_code, 
+        time_frame=time_frame, 
+        interval_from=interval_from, 
+        interval_to=interval_to, 
+        interval_count=interval_count
+    )
+    print(f"Информация о дневных свечах: {day_candles_info}")
+
+# Проверка, что скрипт запущен как основная программа, и запуск асинхронной функции main.
 if __name__ == "__main__":
     asyncio.run(main())
